0ac6a85c3f72c927b73d17108e47535f18a7fc57
yemicodes <yemicodes@gmail.com>
2021-02-07 05:47:09 +0300

    created Add and remove coins

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import _ from 'lodash';
 
 const cc = require('cryptocompare');
 
 
 export const AppContext = React.createContext();
-
+const MAX_FAVORITES = 10;
 export class AppProvider extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             page: 'dashboard',
+            favorites: ['BTC', 'ETH', 'XMR', 'DOGE'],
             ...this.savedSettings(),
             setPage: this.setPage,
+            addCoin: this.addCoin,
+            removeCoin: this.removeCoin,
+            isInFavorites: this.isInFavorites,
             confirmFavorites: this.confirmFavorites
         }
     }
@@ -25,23 +30,39 @@ export class AppProvider extends React.Component {
         this.setState({ coinList });
     }
 
+    addCoin = key => {
+        let favorites = [...this.state.favorites];
+        if (favorites.length < MAX_FAVORITES) {
+            favorites.push(key);
+            this.setState({ favorites });
+        }
+    }
+
+    removeCoin = key => {
+        let favorites = [...this.state.favorites];
+        this.setState({ favorites: _.pull(favorites, key) });
+    }
+
+    isInFavorites = key => _.includes(this.state.favorites, key)
+
     confirmFavorites = () => {
         this.setState({
             firstVisit: false,
             page: 'dashboard'
         });
         localStorage.setItem('tradeIndex', JSON.stringify({
-            test: 'hello'
+            favorites: this.state.favorites
         }));
 
     }
 
     savedSettings() {
-        let cryptoDashData = JSON.parse(localStorage.getItem('tradeIndex'));
-        if (!cryptoDashData) {
+        let tradeIndexData = JSON.parse(localStorage.getItem('tradeIndex'));
+        if (!tradeIndexData) {
             return { page: 'settings', firstVisit: true }
         }
-        return {};
+        let { favorites } = tradeIndexData;
+        return {favorites};
     }
 
     setPage = page => this.setState({ page })
--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -4,18 +4,33 @@ import { SelectableTile, DisabledTile, TileDelete } from "../Shared/Tile";
 import CoinImage from '../Shared/CoinImage';
 import CoinHeaderGrid from './CoinHeaderGrid';
 
+function clickCoinHandler(topSection, coinKey, addCoin, removeCoin) {
+    return topSection ? () => {
+        removeCoin(coinKey)
+    } : () => {
+        addCoin(coinKey)
+    }
+}
+
 // eslint-disable-next-line
-export default function ({ coinKey, topSection }) {
+export default function ({ coinKey, topSection}) {
     return <AppContext.Consumer>
-        {({ coinList }) => {
+        {({ coinList, addCoin, removeCoin, isInFavorites }) => {
             let coin = coinList[coinKey];
  
             let TileClass = SelectableTile;
             if (topSection) {
                 TileClass = TileDelete;
+            } else if (isInFavorites(coinKey)) {
+                TileClass = DisabledTile;
             }
-            return <TileClass>
-                <CoinHeaderGrid topSection={topSection} name={coin.CoinName} symbol={coin.Symbol} />
+            return <TileClass
+                onClick={clickCoinHandler(topSection, coinKey, addCoin, removeCoin)}
+            >
+                <CoinHeaderGrid
+                    topSection={topSection} 
+                    name={coin.CoinName}
+                    symbol={coin.Symbol} />
                 <CoinImage coin={coin} />
             </TileClass>
         }}
