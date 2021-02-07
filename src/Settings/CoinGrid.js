6cae79673d06039f8ed172c310753f7bf11e4213
yemicodes <yemicodes@gmail.com>
2021-02-07 08:43:08 +0300

    added fuzzy chat

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -6,19 +6,22 @@ import CoinTile from "./CoinTile";
 export const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
-    grid-gap: 50px;
+    grid-gap: 15px;
     margin-top: 40px;
 `
+function getLowerSectionCoins(coinList, filteredCoins) {
+    return (filteredCoins && Object.keys(filteredCoins)) || Object.keys(coinList).slice(0, 100)
+}
 
-function getCoinsToDisplay(coinList, topSection, favorites) {
-    return topSection ? favorites : Object.keys(coinList).slice(0, 70);
+function getCoinsToDisplay(coinList, topSection, favorites, filteredCoins) {
+    return topSection ? favorites : getLowerSectionCoins(coinList, filteredCoins);
 }
 
 // eslint-disable-next-line
 export default function({topSection}) {
     return <AppContext.Consumer>
-        {({ coinList, favorites }) => <CoinGridStyled>
-            {getCoinsToDisplay(coinList, topSection, favorites).map(coinKey =>
+        {({ coinList, favorites, filteredCoins }) => <CoinGridStyled>
+            {getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey =>
                 <CoinTile topSection={topSection} coinKey={coinKey} />
             )}
         </CoinGridStyled>}
