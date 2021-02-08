f7a404d7ab70f0c181d3c13496393a333cc14a47
yemicodes <yemicodes@gmail.com>
2021-02-08 07:19:19 +0300

    added highcharts

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
-import Page from '../Shared/Page';
-import PriceGrid from './PriceGrid';
-import CoinSpotlight from './CoinSpotlight';
-
+import React from "react";
+import styled from "styled-components";
+import Page from "../Shared/Page";
+import PriceGrid from "./PriceGrid";
+import CoinSpotlight from "./CoinSpotlight";
+import PriceChart from "./PriceChart";
 
 const ChartGrid = styled.div`
-    display: grid;
-    margin-top: 20px;
-    grid-gap: 15px;
-    grid-template-columns: 1fr 3fr;
-`
+  display: grid;
+  margin-top: 20px;
+  grid-gap: 15px;
+  grid-template-columns: 1fr 3fr;
+`;
 
 // eslint-disable-next-line
 export default function () {
-    return <Page name="dashboard">
-        <PriceGrid />
-        <ChartGrid>
-            <CoinSpotlight />
-            <div> Chart goes here </div>
-        </ChartGrid>
+  return (
+    <Page name="dashboard">
+      <PriceGrid />
+      <ChartGrid>
+        <CoinSpotlight />
+        <PriceChart />
+      </ChartGrid>
     </Page>
+  );
 }
